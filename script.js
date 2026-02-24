3532376f1a71e658dcbad01afb2f738305dfefbb
Artem322-v2 <Zakiev2100@mail.ru>
2026-02-24 12:52:39 +0400

    Выполненое задание

---
+++ script.js
@@ -0,0 +1,64 @@
+// Задание 1
+
+function timer(seconds) {
+    let remaining = seconds;
+
+    function tick() {
+        console.log(`Осталось: ${remaining} сек`);
+        remaining--;
+
+        if (remaining >= 0) {
+            setTimeout(tick, 1000); 
+        } else {
+            console.log("Время вышло!");
+        }
+    }
+
+    tick(); 
+
+};
+
+timer(10);
+
+// Задание 2
+
+function timerWater () {
+    alert ('не забудь выпить воды');
+};
+
+setInterval (timerWater, 30 * 60 *1000);
+
+
+
+
+//задание 3
+const delayInput = document.getElementById('delayInput');
+const textInput = document.getElementById('textInput');
+const beggin = document.getElementById('beggin');
+
+let intervalId = null;
+
+beggin.addEventListener('click', function () {
+    const delayText = delayInput.value;
+    const message = textInput.value;
+
+    const delayMilliseconds = delayText * 1000;
+
+    function greet() {
+
+        console.log('Задержка:', delayText);
+        console.log('Сообщение:', message);
+    }
+
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    } else {
+
+        intervalId = setInterval(greet, delayMilliseconds);
+    }
+
+})
+
+
+
